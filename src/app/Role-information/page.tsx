caa8ccc80ea446d4c2a6993308d3282826b2535b
Anne Maline <annemaline.hjelmervik@gmail.com>
2024-11-27 18:23:24 +0100

    remove role information page

--- src/app/Role-information/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function RoleInformationPage() {
-  return (
-    <div>
-      <h1>Information about the role</h1>
-    </div>
-  );
-}
--- tests/unit/__tests__/pages/roleinformation.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import RoleInformationPage from "../../../../src/app/Role-information/page";
-
-describe("Role-information Page", () => {
-  it("renders correctly", () => {
-    const { container } = render(<RoleInformationPage />);
-    expect(container).toMatchSnapshot();
-  });
-});
