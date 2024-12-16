23dbcf0bff2e6d1a661c54f428c8f89c8db0f4d4
Anne Maline <annemaline.hjelmervik@gmail.com>
2024-12-16 10:21:49 +0100

    simple callback and logout pages

---
+++ src/app/callback/page.tsx
@@ -0,0 +1,9 @@
+const CallbackPage = () => {
+  return (
+    <div>
+      <h1>Callback</h1>
+    </div>
+  );
+};
+
+export default CallbackPage;
---
+++ src/app/logout/page.tsx
@@ -0,0 +1,9 @@
+const LogoutPage = () => {
+  return (
+    <div>
+      <h1>Logging out...</h1>
+    </div>
+  );
+};
+
+export default LogoutPage;
