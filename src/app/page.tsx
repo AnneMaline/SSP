0e8da9256779b485ddd304fb503ef4a69180a478
Anne Maline <annemaline.hjelmervik@gmail.com>
2024-12-19 13:46:04 +0100

    Styles TopTasks and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,17 @@ import TitleBanner from "@/components/TitleBanner";
 export default function Home() {
   const { data: session, status } = useSession();
   // TopTask titles and routes
-  const [topTaskTitle, setTopTaskTitle] = useState([
+  const [topTaskPageTitle, setTopTaskPageTitle] = useState([
     "Entitlements",
     "Onboarding",
   ]);
-  const [topTaskRoutes, setTopTaskRoutes] = useState([
-    "entitlements",
-    "onboarding",
-  ]);
+  const topTaskTitle = ["SELF-SERVICE PORTAL", "GET STARTED", "PLATFORM TEAM"];
+  const topTaskDescription = [
+    "List and create access",
+    "We'll take you through!",
+    "Handle requests",
+  ];
+  const topTaskIcon = ["globe.svg", "globe.svg", "globe.svg"];
 
   useEffect(() => {
     if (status !== "loading" && (!session || !session.accessToken)) {
@@ -26,8 +29,7 @@ export default function Home() {
     if (session && session.accessToken) {
       checkRole(session.accessToken, "bootcamp").then((hasRole) => {
         if (hasRole) {
-          setTopTaskTitle(["Entitlements", "Onboarding", "Requests"]);
-          setTopTaskRoutes(["entitlements", "onboarding", "requests"]);
+          setTopTaskPageTitle(["Entitlements", "Onboarding", "Requests"]);
         }
       });
     }
@@ -37,34 +39,35 @@ export default function Home() {
     "Misson statement text: OSDU Selv-service portal enables you to find, collect and read relevant information for the xx and x This page contains all relevant documentation and provides you with the right tools to share, collect and save data. The self-service portal is under construction and we appreciate eedback for further improvements.";
 
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="bg-[var(--homepage-background)] flex flex-col gap-10 w-full sm:items-start">
       {/* Title */}
       <TitleBanner
         title="OSDU Self-service Portal"
         description={description}
         back={false}
       />
-
-      {/* Login */}
-      <Authentication />
-
       {/* TopTasks */}
-      <ul className="flex justify-end space-x-4">
-        {topTaskTitle.map((text, index) => (
+      <ul className="flex flex-row flex-wrap gap-3 pl-10">
+        {topTaskPageTitle.map((title, index) => (
           <li key={index}>
-            <TopTask targetUrl={`/${topTaskRoutes[index]}`} text={`${text}`} />
+            <TopTask
+              targetUrl={`/${title.toLowerCase()}`}
+              title={topTaskTitle[index]}
+              pageTitle={title}
+              description={topTaskDescription[index]}
+              icon={topTaskIcon[index]}
+            />
           </li>
         ))}
       </ul>
 
       {/* Further documentation - OSDU documentation */}
-      <h1>Further documentation</h1>
-      <a
-        href="https://docs.osdu.equinor.com/"
-        className={`px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600`}
-      >
-        OSDU Documentation
-      </a>
+      <p className="pl-10 pb-4">
+        If you are interested in further documentation.{" "}
+        <a href="https://docs.osdu.equinor.com/" className="text-blue-700">
+          Go to OSDU Documentation.
+        </a>
+      </p>
     </main>
   );
 }
--- src/components/TopTask.tsx
+++ src/components/TopTask.tsx
@@ -1,19 +1,39 @@
 import Link from "next/link";
-import React from "react";
+import styles from "./TopTask.module.css";
 
 interface TopTaskProps {
-  text: string; // Text on the top task
-  targetUrl: string; // target URL for navigation
-  className?: string; // Optional className prop for customization
+  title: string; // Title on the top task
+  pageTitle: string; // The page title
+  description: string; // Description of what the page does
+  targetUrl: string; // Target URL for navigation
+  icon: string; // File to icon for the top task
 }
 
-const TopTask = ({ text, targetUrl, className = "" }: TopTaskProps) => {
+const TopTask = ({
+  title,
+  pageTitle,
+  description,
+  targetUrl,
+  icon,
+}: TopTaskProps) => {
   return (
-    <Link
-      href={`${targetUrl}`}
-      className={`px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 ${className}`}
-    >
-      {text}
+    <Link href={targetUrl} className={styles.container}>
+      <h1
+        style={{ fontSize: "24px", marginBottom: "10px" }}
+        className="text-title"
+      >
+        {title}
+      </h1>
+      <hr className={styles.separator} />
+      <div className={styles.content}>
+        <div className={styles.icon_circle}>
+          <img src={icon} alt="icon" width={20} />
+        </div>
+        <div className={styles.content_text}>
+          <p className="text-bold">{pageTitle}</p>
+          <p className="text-small">{description}</p>
+        </div>
+      </div>
     </Link>
   );
 };
